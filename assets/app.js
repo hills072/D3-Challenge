c11f4297aed72ff3f140852fd6a6db1cdb099229
Mike Hills <hills072@umn.edu>
2021-05-14 17:37:18 -0500

    created scales, axes, and appended axes to group

--- assets/app.js
+++ assets/app.js
@@ -49,7 +49,34 @@ d3.csv("assets/data.csv").then(function(demoData, err) {
     console.log(poverty);
     console.log(healthcare);
     console.log(states);
+    var povertyMax = d3.max(demoData, d => d.poverty);
+    var healthcareMax = d3.max(demoData, d => d.healthcare);
+    console.log(povertyMax);
+    console.log(healthcareMax);
 
+    // create scales
+
+    var xScale = d3.scaleLinear()
+        .domain([0, povertyMax])
+        .range([0, width]);
+    
+    var yScale = d3.scaleLinear()
+        .domain([0, healthcareMax])
+        .range([height, 0]);
+    
+    // create axes
+
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+
+    // append axes
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+      chartGroup.append("g")
+        .call(yAxis);
 
 
     });
